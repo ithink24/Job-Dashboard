498106e3d375ef4ddf596ef49409c939c3d7cd20
ithink24 <2401ankitrajpoot@gmail.com>
2023-12-08 16:33:45 +0530

    add create job button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import JobCard from './JobCard';
 
-
+const arr = [1, 2, 3, 4];
 function App() {
-    const arr = [1, 2, 3, 4];
+    const[open, setOpen] = useState(false);
+    console.log(open, 'ooo');
+
     return (
         <div className="w-[100%] px-6 py-4 relative bg-zinc-300">
+            <div className="px-4 py-2 mb-4 bg-sky-500 rounded-md shadow w-fit cursor-pointer">
+                <button onClick={() => setOpen(!open)}>+ Create New Job</button>
+            </div>
+
             <div className="flex flex-wrap gap-7">
                 {arr.map((item) => (
                     <JobCard/>
